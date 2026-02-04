a5db86cfebdea4a9005c8eed353936654a589094
nikolas <nikosnick13@live.com>
2026-02-04 10:14:21 +0200

    Initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
@@ -0,0 +1,31 @@
+package gr.aueb.cf.schoolapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
+
+    @CreatedDate
+    @Column(name="created_at",nullable = false, columnDefinition = "DATETIME")
+    private Instant createdAt;
+
+    @Column(name="update_at",nullable = false, columnDefinition = "DATETIME")
+    private Instant updateAt;
+
+
+}
---
+++ src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.schoolapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+@Table(name = "teachers")
+public class Teacher extends AbstractEntity {
+}
