a3cdfae185dd342d002c566318c3c0180e68e17e
nikolas <nikosnick13@live.com>
2026-02-17 11:11:30 +0200

    chore: add localization login

--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
@@ -5,20 +5,26 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record TeacherInsertDTO(
+
         //BeanValidation
-        @NotNull(message = "Το όνομα δεν πρέπει αν είναι κενό")
-        @Size(min=2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον 2 χαρακτίρες")
+        // @NotNull(message = "Το όνομα δεν πρέπει αν είναι κενό")
+        //@Size(min=2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον 2 χαρακτίρες")
+        @NotNull
+        @Size
         String firstname,
 
-        @NotNull(message = "Το επώνυμο δεν πρέπει αν είναι κενό")
-        @Size(min=2, message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον 2 χαρακτίρες")
+        //@NotNull(message = "Το επώνυμο δεν πρέπει αν είναι κενό")
+        //@Size(min=2, message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον 2 χαρακτίρες")
+        @NotNull
+        @Size
         String lastname,
 
-
-        @Pattern(regexp = "\\d{9,}", message = "Το ΑΦΜ δεν πρέπει να είναι μικρότερο απο 9 ψηφία" )
+        //@Pattern(regexp = "\\d{9,}", message = "Το ΑΦΜ δεν πρέπει να είναι μικρότερο απο 9 ψηφία" )
+        @Pattern(regexp = "\\d{9,}")
         String vat,
 
-        @NotNull(message = "Η περιοxή δεν πρέπει να ειναι κενή")
+        //@NotNull(message = "Η περιοxή δεν πρέπει να ειναι κενή")
+        @NotNull
         Long regionId
 
     ) {
