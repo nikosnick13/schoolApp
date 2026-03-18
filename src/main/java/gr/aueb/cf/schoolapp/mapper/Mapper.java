8e9b8d16298a680bd2bcb07324bef0509d468f17
nikolas <nikosnick13@live.com>
2026-03-18 14:10:47 +0200

    chore: add mapping mapToUserEntity and mapToUserReadOnlyDTO

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -1,12 +1,12 @@
 package gr.aueb.cf.schoolapp.mapper;
 
-import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherEditDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.dto.*;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.model.User;
 import gr.aueb.cf.schoolapp.model.static_data.Region;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class Mapper {
@@ -25,5 +25,12 @@ public class Mapper {
 
     public TeacherEditDTO mapToTeacherEditDTO(Teacher teacher){
         return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(), teacher.getLastname(), teacher.getVat(),teacher.getRegion().getId());
+    }
+
+    public User mapToUserEntity(UserInsertDTO userInsertDTO){
+        return new User(userInsertDTO.username(),userInsertDTO.password());
+    }
+    public UserReadOnlyDTO mapToUserReadOnlyDTO(User user) {
+        return new UserReadOnlyDTO(user.getUuid().toString(), user.getUsername(), user.getRole().getName());
     }
 }
