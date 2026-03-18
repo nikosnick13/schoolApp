53f87c5675f18340156e7fcfa6e0a4bbee80864a
nikolas <nikosnick13@live.com>
2026-03-18 14:40:12 +0200

    feat: add role contract, service and dto

---
+++ src/main/java/gr/aueb/cf/schoolapp/service/IRoleService.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.schoolapp.service;
+
+import gr.aueb.cf.schoolapp.dto.RoleReadOnlyDTO;
+
+import java.util.List;
+
+public interface IRoleService  {
+
+    List<RoleReadOnlyDTO> findAllRolesSortedByName();
+}
---
+++ src/main/java/gr/aueb/cf/schoolapp/service/RoleService.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.schoolapp.service;
+
+import gr.aueb.cf.schoolapp.dto.RoleReadOnlyDTO;
+import gr.aueb.cf.schoolapp.mapper.Mapper;
+import gr.aueb.cf.schoolapp.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RoleService implements IRoleService{
+
+private final RoleRepository roleRepository;
+private final Mapper mapper;
+
+
+    @Override
+    public List<RoleReadOnlyDTO> findAllRolesSortedByName() {
+        return roleRepository.findAllByOrderByNameAsc()
+                .stream()
+                .map(mapper::mapToRoleReadOnlyDTO)
+                .toList();
+    }
+}
